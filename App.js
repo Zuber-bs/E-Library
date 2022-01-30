1680c52753c945555f23a5e8d304ae785636f439
Pranav Shishodia <pranavshishodia11105@gmail.com>
2022-01-29 18:52:50 -0600

    Create Bottom Tab Navigator

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native'
+import BottomTabNavigator from './navigation/BottomTabNavigator';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <BottomTabNavigator />
+    );
+  }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
---
+++ navigation/BottomTabNavigator.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import TransactionScreen  from '../screens/TransactionScreen';
+import SearchScreen from '../screens/SearchScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default class BottomTabNavigator extends React.Component {
+  render() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Transaction" component={TransactionScreen}/>
+                <Tab.Screen name="Search" component={SearchScreen}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+  }
+}
+
+
