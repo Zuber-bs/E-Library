f274141aa5c02c29815c55b2c59ce5f2bf2e4e21
Pranav Shishodia <pranavshishodia1105@gmail.com>
2022-02-27 19:15:50 -0600

    Scan QR Code

--- screens/TransactionScreen.js
+++ screens/TransactionScreen.js
@@ -1,13 +1,53 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as Permissions from "expo-permissions";
 
 export default class TransactionScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      domState: "normal",
+      scannedData: null,
+      scanned: false,
+      cameraPermission: false,
+    }
+  }
+
+  handleBarcodeScan = async({type, data}) => {
+    this.setState({
+      domState: "normal",
+      scannedData: data,
+      scanned: true,
+    });
+  }
+
+  getCameraPermissions = async(domState) => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      cameraPermission: status === "granted",
+      domState: domState,
+    });
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Transaction</Text>
-      </View>
-    );
+    const {domState, scannedData, scanned, cameraPermission} = this.state;
+    if(domState == "scanning") {
+      return <BarCodeScanner onBarCodeScanned={scanned ? undefined : this.handleBarcodeScan} style={StyleSheet.absoluteFillObject}/>;
+    } else {
+      return (
+        <View style={styles.container}>
+  
+          <Text>{cameraPermission ? scannedData : "Please allow camera"}</Text>
+  
+          <TouchableOpacity style={styles.button} onPress={() => {
+            this.getCameraPermissions("scanning");
+          }}>
+            <Text style={styles.text}>Scan QR Code</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
   }
 }
 
@@ -16,5 +56,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+
+  button: {
+    backgroundColor: "dodgerblue",
+    padding: 20,
+    borderRadius: 5,
+  },
+
+  text: {
+    color: "white",
+    fontWeight: "bold",
   }
 });
